c65fd11994349ca610c41e5095a85aae2407ceaf
tainacbueno <56885213+tainacbueno@users.noreply.github.com>
2019-10-24 15:38:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -32,11 +32,13 @@ int ChamaIup(int argc, char **argv){
     Ihandle *dlg, *vbox, *button_exit, *button_deposita, *button_saque, *label;
     Ihandle *transacoes_menu, *movimenta_menu;
     Ihandle *item_coloca, *item_retira;
-    Ihandle *item_moradia, *item_estudo, *item_tranporte, *item_alimentcao, *item_trabalho;
+    Ihandle *item_moradia, *item_estudo, *item_tranporte, *item_alimentcao, *item_trabalho, *item_geral;
     Ihandle *item_12mes, *item_categoriames;
     Ihandle *sub_menu_transacoes, *sub_menu_movimenta, *menu;
 
     IupOpen(&argc, &argv);
+
+    /*/////////////////////////// MENU ////////////////////////////////////*/
 
     item_coloca = IupItem("Depositar", NULL);
     item_retira = IupItem("Sacar", NULL);
@@ -46,6 +48,7 @@ int ChamaIup(int argc, char **argv){
     item_tranporte = IupItem("Transporte", NULL);
     item_alimentcao = IupItem("Alimentação", NULL);
     item_trabalho = IupItem("Trabalho", NULL);
+    item_geral = IupItem("Geral", NULL);
 
     transacoes_menu = IupMenu(
             item_coloca,
@@ -59,36 +62,45 @@ int ChamaIup(int argc, char **argv){
             item_tranporte,
             item_alimentcao,
             item_trabalho,
+            item_geral,
             NULL
     );
 
     sub_menu_transacoes = IupSubmenu("Transações", transacoes_menu);
-    sub_menu_movimenta = IupSubmenu("Movimentação", movimenta_menu);
+    sub_menu_movimenta = IupSubmenu("Relatórios", movimenta_menu);
     menu = IupMenu(
             sub_menu_transacoes,
             sub_menu_movimenta,
             NULL
     );
 
-    label = IupLabel("PW\n\n\nBem-vindo a sua carteira pessoal!");
-
-    button_exit = IupButton("SAIR", NULL);
     IupSetCallback(item_coloca, "ACTION", (Icallback)deposita);
     IupSetCallback(item_retira, "ACTION", (Icallback)saque);
-    vbox = IupVbox(label, button_exit,  NULL);
+
+    /*/////////////////////////// MENU ////////////////////////////////////*/
+
+    //mensagem inicial
+    label = IupLabel("Bem-vindo(a) à sua carteira pessoal!");
+    IupSetAttribute(label, "FONTSIZE", "20");
+
+    //botão saída
+    button_exit = IupButton("SAIR", NULL);
+    IupSetCallback(button_exit, "ACTION", (Icallback)botao_exit);
+    IupSetAttribute(button_exit, "SIZE", "35x20");
+
+    //vbox
+    vbox = IupVbox(
+            label,
+            button_exit,
+            NULL);
+    IupSetAttribute(vbox, "ALIGNMENT", "ACENTER");
+    IupSetAttribute(vbox, "MARGIN", "200x200");
+    IupSetAttribute(vbox, "GAP", "40");
 
     dlg = IupDialog(vbox);
 
-    IupSetAttribute(dlg, "TITLE", "Carteirinha");
+    IupSetAttribute(dlg, "TITLE", "Carteira Pessoal");
     IupSetAttributeHandle(dlg, "MENU", menu);
-    IupSetAttribute(label, "ALIGNMENT", "ACENTER");
-
-    IupSetAttribute(vbox, "GAP", "80");
-    IupSetAttribute(vbox, "MARGIN", "200x150");
-
-    IupSetAttribute(vbox, "ALIGNMENT", "ACENTER");
-    IupSetAttribute(button_exit, "SIZE", "35x20");
-    IupSetCallback(button_exit, "ACTION", (Icallback)botao);
 
     IupShowXY(dlg, IUP_CENTER, IUP_CENTER);
 
